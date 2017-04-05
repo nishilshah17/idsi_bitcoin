7801ba848d0a39d6bdb782c4c2d1cea101b928cf
= <nishil.shah17@gmail.com>
2017-04-05 20:08:17 +0000

    more refactoring

--- reduce_blockchain/ReduceBlockchain.java
+++ reduce_blockchain/ReduceBlockchain.java
@@ -43,78 +43,78 @@ import datatypes.TransactionWritable;
 
 public class ReduceBlockchain {
 
-  public static class BlockMapper extends Mapper<NullWritable, BytesWritable, BlockWritable, TransactionWritable> {
+    public static class BlockMapper extends Mapper<NullWritable, BytesWritable, BlockWritable, TransactionWritable> {
 
-    private Text word = new Text();
+        private Text word = new Text();
 
-    public void map(NullWritable key, BytesWritable value, Context context) throws IOException, InterruptedException {
-        //get block
-        Block block = BlockUtils.getBlock(value.getBytes());
-        BlockWritable bw = new BlockWritable(block);
+        public void map(NullWritable key, BytesWritable value, Context context) throws IOException, InterruptedException {
+            //get block
+            Block block = BlockUtils.getBlock(value.getBytes());
+            BlockWritable bw = new BlockWritable(block);
 
-        //get transactions
-        for(Transaction tx : block.getTransactions()) {
-            context.write(bw, new TransactionWritable(tx));
+            //get transactions
+            for(Transaction tx : block.getTransactions()) {
+                context.write(bw, new TransactionWritable(tx));
+            }
         }
     }
-  }
 
-  public static class BlockReducer extends Reducer<BlockWritable, TransactionWritable, Text, IntWritable> {
-    private IntWritable one = new IntWritable(1);
-    private Text blockTag = new Text("block");
-    private Text transactionTag = new Text("transaction");
+    public static class BlockReducer extends Reducer<BlockWritable, TransactionWritable, Text, IntWritable> {
+        private IntWritable one = new IntWritable(1);
+        private Text blockTag = new Text("block");
+        private Text transactionTag = new Text("transaction");
 
-	private MultipleOutputs multipleOutputs;
+        private MultipleOutputs multipleOutputs;
 
-	public void setup(Context context) {
-		multipleOutputs = new MultipleOutputs(context);
-	}
+        public void setup(Context context) {
+            multipleOutputs = new MultipleOutputs(context);
+        }
 
-    public void reduce(BlockWritable key, Iterable<TransactionWritable> values, Context context) throws IOException, InterruptedException {
-	  String outputFileName = getOutputFileName(key);
-      multipleOutputs.write(key.toText(), one, outputFileName);
-      for (TransactionWritable tx : values) {
-      	multipleOutputs.write(tx.toText(), one, outputFileName);
-	  }
+        public void reduce(BlockWritable key, Iterable<TransactionWritable> values, Context context) throws IOException, InterruptedException {
+            String outputFileName = getOutputFileName(key);
+            multipleOutputs.write(key.toText(), one, outputFileName);
+            for (TransactionWritable tx : values) {
+                multipleOutputs.write(tx.toText(), one, outputFileName);
+            }
+        }
+
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            multipleOutputs.close();
+        }
+
+        public String getOutputFileName(BlockWritable key) {
+            String fileName;
+            try {
+                String time = key.getTime();
+                DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy");
+                Date date = dateFormat.parse(time);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                String month = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)];
+                fileName = month + "-" + calendar.get(Calendar.YEAR);
+            } catch (ParseException pe) {
+                fileName = "date-fail";
+            }
+            return fileName;
+        }
     }
 
-	public void cleanup(Context context) throws IOException, InterruptedException {
-		multipleOutputs.close();
-	}
-
-	public String getOutputFileName(BlockWritable key) {
-		String fileName;
-		try {
-			String time = key.getTime();
-			DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy");
-			Date date = dateFormat.parse(time);
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTime(date);
-            String month = new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)];
-			fileName = month + "-" + calendar.get(Calendar.YEAR);
-		} catch (ParseException pe) {
-			fileName = "date-fail";
-		}
-	  	return fileName;
-	}
-  }
-
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "reduce blockchain");
-	//include in classpath
-    job.setJar("rbc.jar");
-    job.addFileToClassPath(new Path("/user/nishil/bitcoin/bitcoinj.jar"));
-    job.setMapperClass(BlockMapper.class);
-    job.setReducerClass(BlockReducer.class);
-    job.setMapOutputKeyClass(BlockWritable.class);
-    job.setMapOutputValueClass(TransactionWritable.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    job.setInputFormatClass(BlockFileInputFormat.class);
-	job.setOutputFormatClass(TextOutputFormat.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "reduce blockchain");
+        //include in classpath
+        job.setJar("rbc.jar");
+        job.addFileToClassPath(new Path("/user/nishil/bitcoin/bitcoinj.jar"));
+        job.setMapperClass(BlockMapper.class);
+        job.setReducerClass(BlockReducer.class);
+        job.setMapOutputKeyClass(BlockWritable.class);
+        job.setMapOutputValueClass(TransactionWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setInputFormatClass(BlockFileInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
