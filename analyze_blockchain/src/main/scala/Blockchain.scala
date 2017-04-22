98a47d7623eb2f6d4f549cb7a556bfc427a38083
nishilshah17 <nishil.shah17@gmail.com>
2017-04-22 18:26:57 -0500

    new addresses per month

--- analyze_blockchain/src/main/scala/Blockchain.scala
+++ analyze_blockchain/src/main/scala/Blockchain.scala
@@ -17,8 +17,10 @@ object Blockchain {
     val sc = new SparkContext(conf)
 
     val dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy")
-    val blockFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_data/*/*-blocks-r-00000"
-    val transactionFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_data/*/*-transactions-r-00000"
+    val newFormat = new SimpleDateFormat("MM yyyy")
+
+    val blockFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_data/*-blocks-r-00000"
+    val transactionFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_data/*-transactions-r-00000"
     val outputFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_output/new_addresses.txt"
 
     val blockLines = sc.textFile(blockFilePath)
@@ -26,13 +28,12 @@ object Blockchain {
     val blocks = blockLines.map(line => line.split(",")).map(arr => (arr(0), dateFormat.parse(arr(3))))
     val transactions = transactionLines.map(line => line.split(","))
       .flatMap(arr => arr(6).split(":").map(address => (arr(0), address))).filter(tx => tx._2 != "null")
-    val addressFirstSeen = blocks.join(transactions).map(entry => (entry._2._2, (entry._1, entry._2._1)))
-      .reduceByKey((a, b) => if(a._2.before(b._2)) a else b)
-    val blocksFirstSeen = addressFirstSeen.map(entry => (entry._2, 1)).reduceByKey(_ + _)
-      .map(entry => (entry._1._1, entry._1._2, entry._2)).collect()
+    val addressFirstSeen = blocks.join(transactions).map(entry => (entry._2._2, entry._2._1))
+      .reduceByKey((a, b) => if(a.before(b)) a else b).map(entry => (entry._1, newFormat.format(entry._2)))
+    val addressesPerMonth = addressFirstSeen.map(entry => (entry._2, 1)).reduceByKey(_ + _).collect()
 
     printToFile(new File(outputFilePath)) { printer =>
-      blocksFirstSeen.foreach(printer.println)
+      addressesPerMonth.foreach(printer.println)
     }
   }
 }
--- sample_output/new_addresses.py
+++ sample_output/new_addresses.py
@@ -3,14 +3,13 @@ from bokeh.plotting import figure, output_file, show
 
 inputFilePath = "new_addresses.txt"
 data = [entry.strip("()\n").split(",") for entry in open(inputFilePath)]
-data = [(entry[0], parse(entry[1]), entry[2]) for entry in data]
-data = sorted(data, key=lambda x: x[1])
+data = [(parse(entry[0]), entry[1]) for entry in data]
+data = sorted(data, key=lambda x: x[0])
 
-blocks = [entry[0] for entry in data]
-times = [entry[1] for entry in data]
-nums = [entry[2] for entry in data]
+months = [entry[0] for entry in data]
+counts = [entry[1] for entry in data]
 
 output_file("new_addresses.html")
-p = figure(title = "New Addresses Per Block", x_axis_label = "Block", y_axis_label = "# of Addresses", x_axis_type = "datetime")
-p.line(times, nums, line_width = 2)
+p = figure(title = "New Addresses Per Month", x_axis_label = "Time", y_axis_label = "# of Addresses", x_axis_type = "datetime")
+p.line(months, counts, line_width = 2)
 show(p)
