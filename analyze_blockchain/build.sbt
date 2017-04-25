7c2c73bc1b15dec9842a9a36dfe0c7371b9c1ffd
nishilshah17 <nishil.shah17@gmail.com>
2017-04-24 21:05:30 -0500

    project

--- analyze_blockchain/build.sbt
+++ analyze_blockchain/build.sbt
@@ -7,8 +7,29 @@ val sparkVersion = "2.1.0"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion, //% "provided"
-  "org.apache.spark" %% "spark-sql" % sparkVersion //% "provided"
+  "org.apache.spark" %% "spark-sql" % sparkVersion, //% "provided"
+  "org.apache.spark" %% "spark-yarn" % sparkVersion //% "provided"
   //"org.apache.spark" % "spark-hive_2.10" % "2.1.0",
-  //"org.apache.spark" % "spark-yarn_2.10", % "2.1.0"
 )
-    
+
+assemblyMergeStrategy in assembly := {
+  case PathList("org","aopalliance", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "inject", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
+  case PathList("org", "apache", xs @ _*) => MergeStrategy.last
+  case PathList("com", "google", xs @ _*) => MergeStrategy.last
+  case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+  case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
+  case PathList("com", "yammer", xs @ _*) => MergeStrategy.last
+  case "about.html" => MergeStrategy.rename
+  case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+  case "META-INF/mailcap" => MergeStrategy.last
+  case "META-INF/mimetypes.default" => MergeStrategy.last
+  case "plugin.properties" => MergeStrategy.last
+  case "log4j.properties" => MergeStrategy.last
+  case x if x.endsWith(".html") => MergeStrategy.discard
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
--- analyze_blockchain/src/main/scala/Blockchain.scala
+++ analyze_blockchain/src/main/scala/Blockchain.scala
@@ -4,13 +4,12 @@
 import org.apache.spark.SparkContext;
 import org.apache.spark.SparkConf;
 import java.text.SimpleDateFormat;
-import java.io._;
 
 object Blockchain {
-  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+/*  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
     val p = new java.io.PrintWriter(f)
     try { op(p) } finally { p.close() }
-  }
+  }*/
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Blockchain").setMaster("local")
@@ -19,9 +18,11 @@ object Blockchain {
     val dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy")
     val newFormat = new SimpleDateFormat("MM yyyy")
 
-    val blockFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_data/*-blocks-r-00000"
-    val transactionFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_data/*-transactions-r-00000"
-    val outputFilePath = "/Users/NishilShah/workspace/idsi_bitcoin/sample_output/new_addresses.txt"
+    //local input path: /Users/NishilShah/workspace/idsi_bitcoin/sample_data
+    val blockFilePath = "hdfs://" + args(0) + "/*-blocks-r-00000"
+    val transactionFilePath = "hdfs://" + args(0) + "/*-transactions-r-00000"
+    //local output path: /Users/NishilShah/workspace/idsi_bitcoin/sample_output
+    val outputFilePath = "hdfs://" + args(1) + "/new_addresses"
 
     val blockLines = sc.textFile(blockFilePath)
     val transactionLines = sc.textFile(transactionFilePath)
@@ -30,10 +31,7 @@ object Blockchain {
       .flatMap(arr => arr(6).split(":").map(address => (arr(0), address))).filter(tx => tx._2 != "null")
     val addressFirstSeen = blocks.join(transactions).map(entry => (entry._2._2, entry._2._1))
       .reduceByKey((a, b) => if(a.before(b)) a else b).map(entry => (entry._1, newFormat.format(entry._2)))
-    val addressesPerMonth = addressFirstSeen.map(entry => (entry._2, 1)).reduceByKey(_ + _).collect()
-
-    printToFile(new File(outputFilePath)) { printer =>
-      addressesPerMonth.foreach(printer.println)
-    }
+    val addressesPerMonth = addressFirstSeen.map(entry => (entry._2, 1)).reduceByKey(_ + _)
+      .repartition(1).saveAsTextFile(outputFilePath)
   }
 }
